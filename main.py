57dcbe7cb87f77241b1c2dcde9e771cbfb256d23
Vitalii Bielimov <vbit2000@gmail.com>
2023-11-28 11:28:01 +0200

    completed module development

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
 """
-Homework Assignment. Python Core. Module 10
+Homework Assignment. Python Core. Module 11
 For a description of the task, please refer to the README.md file
 """
 
+from datetime import datetime
 from collections import UserDict
-from datetime import datetime, timedelta
-from typing import Optional
 
 
 class Field:
     """
     Базовий клас для полів запису.
-    Буде батьківським для всіх полів, у ньому реалізується логіка загальна для всіх полів
+    Буде батьківським для всіх полів.
     """
     def __init__(self, value):
-        self._value = None
         self.value = value
-
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, new_value):
-        self.validate(new_value)
-        self._value = new_value
-
-    def validate(self, value):
-        # Базова валідація, можна розширити в похідних класах
-        pass
 
     def __str__(self):
         return str(self.value)
@@ -39,45 +24,68 @@ class Name(Field):
     Клас для зберігання імені контакту.
     Обов'язкове поле.
     """
-    def __init__(self, value):
-        if not value.isalpha():
-            raise ValueError("Invalid name: name should consist of letters only")
-        super().__init__(value)
+    def __init__(self, name):
+        self._name = None
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
 
 
 class Phone(Field):
     """
     Клас для зберігання номера телефону.
     Має валідацію формату (10 цифр).
-    Необов'язкове поле з телефоном та таких один запис Record може містити декілька.
+    Необов'язкове поле з телефоном. Один запис Record може містити декілька.
     """
-    # def __init__(self, value):
-    #     if not value.isdigit() or len(value) != 10:
-    #         raise ValueError("Invalid phone: phone should consist of 10 digits only")
-    #     # super().__init__(value)
-    #     super().validate(value)
+    def __init__(self, value):
+        self._value = None
+        self.value = value
 
-    def validate(self, value):
-        if not value.isdigit() or len(value) != 10:
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_phone):
+        if self.check_number(new_phone):
+            self._value = new_phone
+        else:
             raise ValueError("Invalid phone: phone should consist of 10 digits only")
-        super().validate(value)
+
+    @staticmethod
+    def check_number(phone_number):
+        return len(phone_number) == 10 and phone_number.isdigit()
 
 
 class Birthday(Field):
     """
-    Клас для зберігання дня народження контакту.
-    Може бути тільки одне таке поле у записі.
+    Клас "Дні народження"
     """
-    def __init__(self, value):
-        try:
-            date_obj = datetime.strptime(value, '%Y-%m-%d')
-        except ValueError:
-            raise ValueError("Invalid birthday format. Please use 'YYYY-MM-DD'")
-        
-        super().__init__(date_obj)
+    def __init__(self, birthday):
+        self._birthday = None
+        self.birthday = birthday
+
+    form = '%Y-%m-%d'
+
+    @property
+    def birthday(self):
+        return self._birthday
+
+    @birthday.setter
+    def birthday(self, new_bd):
+        self._birthday = datetime.strptime(new_bd, self.form)
 
     def __str__(self):
-        return self.value.strftime('%Y-%m-%d')
+        if self._birthday:
+            return self._birthday.strftime(self.form)
+        else:
+            return "Birthday not set!"
 
 
 class Record:
@@ -85,52 +93,92 @@ class Record:
     Клас для зберігання інформації про контакт, включаючи ім'я та список телефонів.
     Відповідає за логіку додавання/видалення/редагування необов'язкових полів та зберігання обов'язкового поля Name
     """
-    def __init__(self, name, birthday: Optional[Birthday] = None):
+    def __init__(self, name, birthday=None):
         self.name = Name(name)
         self.phones = []
-        self.birthday = birthday
-    
+        self.birthday = Birthday(birthday) if birthday else birthday
+
     # Додавання телефонів
-    def add_phone(self, phone):
-        phone = Phone(phone)
-        self.phones.append(phone)
+    def add_phone(self, phone_number):
+        phone = phone_number
+        self.phones.append(Phone(phone))
+
+    # Додавання дня народження
+    def add_birthday(self, bd):
+        self.birthday = Birthday(bd)
+        return self.birthday
+
+    # Повертає кількість днів до наступного дня народження
+    def days_to_bd(self):
+        if not self.birthday:
+            return "Birthday not set"
+
+        now = datetime.now()
+        bd = self.birthday.birthday
+        certain_year = now.year
+        bd = bd.replace(year=certain_year)
+        if bd < now:
+            bd = bd.replace(year=certain_year + 1)
+        days_to_bdd = (bd.date() - now.date()).days
+
+        return f"{days_to_bdd} days before the birthday"
 
     # Видалення телефонів
     def remove_phone(self, phone):
-        self.phones = [num for num in self.phones if num.value != phone]
+        for el in self.phones:
+            if el.value == phone:
+                self.phones.remove(el)
+                return f"Phone {phone} has been deleted"
+        return f"Phone {phone} is not found"
 
     # Редагування телефонів
     def edit_phone(self, old_phone, new_phone):
-        if not self.find_phone(old_phone):
-            raise ValueError("Phone not found")
-        self.remove_phone(old_phone)
-        self.add_phone(new_phone)
+        for ind, phone in enumerate(self.phones):
+            if phone.value == old_phone:
+                self.phones[ind] = Phone(new_phone)
+                return f"Phone number has been updated for {self.name.name}"
+        raise ValueError
 
     # Пошук телефону
-    def find_phone(self, phone):
-        for p in self.phones:
-            if p.value == phone:
-                return p
+    def find_phone(self, phone_to_find):
+        for phone in self.phones:
+            if phone.value == phone_to_find:
+                return phone
         return None
-    
-    # Повертає кількість днів до наступного дня народження
-    def days_to_birthday(self):
-        if not self.birthday:
-            return None
-
-        today = datetime.today()
-        next_birthday = datetime(today.year, self.birthday.value.month, self.birthday.value.day)
-
-        if today > next_birthday:
-            next_birthday = datetime(today.year + 1, self.birthday.value.month, self.birthday.value.day)
-
-        days_remaining = (next_birthday - today).days
-        return days_remaining    
 
     def __str__(self):
-        phones_str = '; '.join(str(p.value) for p in self.phones)
-        return f"Contact name: {self.name.value}, phones: {phones_str}, birthday: {self.birthday}"
+        phone_numbers = ', '.join(str(phone) for phone in self.phones)
+        birthday = self.birthday if self.birthday else "not set"
 
+        return f'{self.name.name} - {phone_numbers}, birthday - {birthday}'
+
+
+class AddressBookIterator:
+    """
+    Генератор за записами AddressBook і за одну ітерацію повертає уявлення для N записів.
+    """
+    def __init__(self, address_book, per_page=10):
+        self.address_book = address_book
+        self.keys = list(address_book.data.keys())
+        self.per_page = per_page
+        self.current_page = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        start_idx = self.current_page * self.per_page
+        end_idx = (self.current_page + 1) * self.per_page
+
+        if start_idx >= len(self.keys):
+            raise StopIteration
+
+        page_keys = self.keys[start_idx:end_idx]
+        page_records = [self.address_book.data[key] for key in page_keys]
+
+        self.current_page += 1
+
+        return page_records
 
 
 class AddressBook(UserDict):
@@ -140,8 +188,8 @@ class AddressBook(UserDict):
     """
 
     # Додавання записів
-    def add_record(self, record):
-        self.data[record.name.value] = record
+    def add_record(self, record: Record):
+        self.data[record.name.name] = record
 
     # Пошук записів за іменем
     def find(self, name):
@@ -150,53 +198,61 @@ class AddressBook(UserDict):
     # Видалення записів за іменем
     def delete(self, name):
         if name in self.data:
-            del self.data[name]
-    
-    # Повертає генератор за записами AddressBook і за одну ітерацію повертає уявлення для N записів
-    def iterator(self, batch_size=1):
-        records = list(self.data.values())
-        for i in range(0, len(records), batch_size):
-            yield records[i:i + batch_size]
+            self.data.pop(name)
+            return f"{name} has been deleted from the AddressBook"
+        return f"{name} is not in the AddressBook"
 
 
 if __name__ == "__main__":
-    # Створення об'єкта класу AddressBook
-    address_book = AddressBook()
+    # Створення нової адресної книги
+    book = AddressBook()
 
-    # Додавання записів до книги
-    record1 = Record(name="JohnDoe", birthday=Birthday("1990-01-01"))
-    record1.add_phone("1234567890")
-    record1.add_phone("9876543210")
-    address_book.add_record(record1)
+    # Створення записів для перевірки
+    john_record = Record("John")
+    john_record.add_phone("1234567890")
+    john_record.add_birthday("2010-11-10")
+    book.add_record(john_record)
 
-    record2 = Record(name="JaneDoe")
-    record2.add_phone("1112223333")
-    address_book.add_record(record2)
+    jane_record = Record("Jane")
+    jane_record.add_phone("9876543210")
+    jane_record.add_birthday("2004-11-11")
+    book.add_record(jane_record)
 
-    # Виведення усіх записів з книги
-    for record_batch in address_book.iterator(batch_size=1):
-        for record in record_batch:
+    alice_record = Record("Alice")
+    alice_record.add_phone("5551112222")
+    book.add_record(alice_record)
+
+    bob_record = Record("Bob")
+    bob_record.add_phone("9998887777")
+    book.add_record(bob_record)
+
+    # Перевірка виведення всіх записів у книзі
+    print("All records in the address book:")
+    for _, record in book.data.items():
+        print(record)
+
+    # Знаходження та редагування телефону для John
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+
+    # Виведення імені запису John
+    print("Name of the John's record:")
+    print(john.name.name)
+
+    # Виведення телефонів запису John
+    print("Phones in the John's record:")
+    for phone in john.phones:
+        print(phone.value)
+
+    # Перевірка кількості днів до наступного дня народження
+    print("\nNumber of days until the next birthday:")
+    for name, record in book.data.items():
+        print(f"{name}: {record.days_to_bd()}")
+
+    # Перевірка AddressBookIterator
+    print("\nIterating through the address book using AddressBookIterator:")
+    iterator = iter(AddressBookIterator(book, 2))
+    for page in iterator:
+        print("Page:")
+        for record in page:
             print(record)
-
-    # Редагування номера телефону
-    record1.edit_phone("1234567890", "9990001111")
-
-    # Виведення усіх записів після редагування
-    for record_batch in address_book.iterator(batch_size=1):
-        for record in record_batch:
-            print(record)
-
-    # Видалення запису з книги
-    address_book.delete("JaneDoe")
-
-    # Виведення усіх записів після видалення
-    for record_batch in address_book.iterator(batch_size=1):
-        for record in record_batch:
-            print(record)
-
-    # Виведення кількості днів до наступного дня народження
-    for record_batch in address_book.iterator(batch_size=1):
-        for record in record_batch:
-            days_to_birthday = record.days_to_birthday()
-            if days_to_birthday is not None:
-                print(f"Days to {record.name.value}'s birthday: {days_to_birthday}")
